dd903fa687f85d7dcb3e8406b20e936f614fe136
Matthew Meredith <merematt@udel.edu>
2023-04-27 14:18:26 -0400

    solved rendezvous problem

--- hw2/rendezvous.c
+++ hw2/rendezvous.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 // If done correctly, each child should print their "before" message
 // before either prints their "after" message. Test by adding sleep(1)
@@ -12,6 +13,9 @@ void *child_1(void *arg)
 {
     printf("child 1: before\n");
     // what goes here?
+    sleep(2);
+    sem_post(&s1);
+    sem_wait(&s2);
     printf("child 1: after\n");
     return NULL;
 }
@@ -20,6 +24,8 @@ void *child_2(void *arg)
 {
     printf("child 2: before\n");
     // what goes here?
+    sem_post(&s2);
+    sem_wait(&s1);
     printf("child 2: after\n");
     return NULL;
 }
@@ -29,6 +35,8 @@ int main(int argc, char *argv[])
     pthread_t p1, p2;
     printf("parent: begin\n");
     // init semaphores here
+    sem_init(&s1, 0, 0);
+    sem_init(&s2, 0, 0);
     pthread_create(&p1, NULL, child_1, NULL);
     pthread_create(&p2, NULL, child_2, NULL);
     pthread_join(p1, NULL);
