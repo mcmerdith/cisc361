78f81b8ef104afadbe9c604b2e1b57d008012755
Matthew Meredith <merematt@udel.edu>
2023-05-10 18:21:13 -0400

    use another semaphore instead of weird yields

--- proj6/testing/3/rendezvous.c
+++ proj6/testing/3/rendezvous.c
@@ -6,7 +6,7 @@
 // before either prints their "after" message. Test by adding sleep(1)
 // calls in various locations.
 
-sem_t *s1, *s2;
+sem_t *s1, *s2, *complete;
 
 void child_1(int t_id)
 {
@@ -16,6 +16,8 @@ void child_1(int t_id)
     sem_signal(s1);
     sem_wait(s2);
     printf("child 1: after\n");
+
+    sem_signal(complete);
 
     t_terminate();
 }
@@ -28,6 +30,8 @@ void child_2(int t_id)
     sem_wait(s1);
     printf("child 2: after\n");
 
+    sem_signal(complete);
+
     t_terminate();
 }
 
@@ -39,15 +43,16 @@ int main(int argc, char *argv[])
     // init semaphores here
     sem_init(&s1, 0);
     sem_init(&s2, 0);
+    sem_init(&complete, -1);
     t_create(child_1, 0, 0);
     t_create(child_2, 1, 0);
 
     // this thread library doesn't have the equivalent for "join" so this will do
-    t_yield();
-    t_yield();
+    sem_wait(complete);
 
     sem_destroy(&s1);
     sem_destroy(&s2);
+    sem_destroy(&complete);
     printf("parent: end\n");
 
     t_shutdown();
